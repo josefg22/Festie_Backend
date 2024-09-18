7c7ac0f68dbbfc70e629beab6b0520c84ce3239a
Alejandro Ros Cordero <AROSCOR@upv.edu.es>
2024-09-18 10:31:54 +0200

    Initial commit

---
+++ src/main/java/com/example/festie_backend/FestieBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.festie_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestieBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestieBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/festie_backend/FestieBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.festie_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestieBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
