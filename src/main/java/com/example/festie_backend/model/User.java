d7fd6c2d4a54e0243b96053d030fc348bf6c4abe
Alejandro Ros Cordero <AROSCOR@upv.edu.es>
2024-10-16 11:56:42 +0200

    Configuración de base de datos y endpoint de Usuarios

--- src/main/java/com/example/festie_backend/model/User.java
+++ src/main/java/com/example/festie_backend/model/User.java
@@ -1,4 +1,24 @@
 package com.example.festie_backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Setter
+@Getter
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private Date birthday;
+
 }
--- src/main/java/com/example/festie_backend/repository/UserRepository.java
+++ src/main/java/com/example/festie_backend/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.example.festie_backend.repository;
 
-public class UserRepository {
+import com.example.festie_backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    //Agregar consultas personalizadas si es necesario
 }
